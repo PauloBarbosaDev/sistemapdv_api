1661aa58a10e3d5050b8077617a32609d604cd96
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-03-05 21:30:55 -0300

    feat: Initial project setup
    Initialized the project with basic folder structure, configuration files, and initial dependencies.

---
+++ config/sequelizeCli.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: '5432',
+    database: 'pdv',
+    username: 'postgres',
+    password: '123456',
+  },
+};
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import { sequelize } from './database';
+import { router } from './routes';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(router);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  sequelize.authenticate().then(() => {
+    console.log('DB connection successfull.');
+  });
+
+  console.log(`Server started successfuly at port ${PORT}.`);
+});
