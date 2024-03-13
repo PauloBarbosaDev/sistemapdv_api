87a649285de92dbfb8a1c80aec33e37f0db87c0f
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-03-13 14:10:03 -0300

    feat(database): Add Sequelize migrations for Order model
    Added migrations to manage changes in the Order model schema.

---
+++ src/database/migrations/20240313024526-table-order.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      customer_id: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+        references: { model: 'customers', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+      observation: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      total_value: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('order');
+  },
+};
---
+++ src/database/migrations/20240313025554-table-order-products.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('order_products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      order_id: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+        references: { model: 'orders', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+      product_id: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+        references: { model: 'products', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      value: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('order_products');
+  },
+};
