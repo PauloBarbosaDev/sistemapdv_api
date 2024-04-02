fdd62294b3d00c3766e6cf544e1fd3ea4dedec7f
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-04-02 01:46:30 -0300

    feat(deploy): Update package.json for deployment

--- config/sequelizeCli.js
+++ config/sequelizeCli.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     dialect: 'postgres',
     host: process.env.DB_HOST,
-    port: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || '5432'),
     database: process.env.DB_DATABASE,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
