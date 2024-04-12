f8183466386d244257a6dfe3d785d30a5e90fbc9
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-04-12 15:11:15 -0300

    feat: add SQL dump file

---
+++ src/sql/dump.sql
@@ -0,0 +1,48 @@
+create table users (
+  id serial primary key,
+  name text not null,
+  email text not null,
+  password text not null
+ );
+
+create table categories (
+  id serial primary key,
+  description text not null
+ );
+
+create table products (
+  id serial primary key,
+  description text,
+  quantity_stock integer,
+  value integer,
+  category_id integer references categories(id),
+  image text
+);
+
+create table customers (
+  id serial primary key,
+  name text, 
+  email text unique,
+  cpf text unique,
+  zipcode integer, 
+  street text,
+  number integer,
+  district text,
+  city text, 
+  state text
+);
+
+create table orders (
+  id serial primary key,
+  customer_id integer not null,
+  observation text,
+  total_value integer not null
+);
+
+create table order_products (
+  id serial primary key,
+  order_id integer not null references orders(id),
+  product_id integer not null references products(id),
+  quantity integer not null,
+  value integer not null 
+);
