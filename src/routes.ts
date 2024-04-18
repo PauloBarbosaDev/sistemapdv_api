13a1b1c68262d8f3ed64f7ea94643f481cb27482
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-04-18 03:55:44 -0300

    fix: Add missing parameter to user update route

--- src/routes.ts
+++ src/routes.ts
@@ -1,79 +1,79 @@
-import express from 'express';
-import { categoriesController } from './controllers/categoriesController';
-import { authController } from './controllers/authController';
-import { usersController } from './controllers/usersController';
-import { ensureAuth } from './middlewares/auth';
-import { productController } from './controllers/productsController';
-import { customerController } from './controllers/customerController';
-import { orderController } from './controllers/orderController';
-import loginSchema from './validation/authSchema';
-import validationBody from './middlewares/validateBody';
-import userSchema from './validation/userSchema';
-import categoriesSchema from './validation/categoriesSchema';
-import productSchema from './validation/productsSchema';
-import orderSchema from './validation/orderSchema';
+import express from "express";
+import { categoriesController } from "./controllers/categoriesController";
+import { authController } from "./controllers/authController";
+import { usersController } from "./controllers/usersController";
+import { ensureAuth } from "./middlewares/auth";
+import { productController } from "./controllers/productsController";
+import { customerController } from "./controllers/customerController";
+import { orderController } from "./controllers/orderController";
+import loginSchema from "./validation/authSchema";
+import validationBody from "./middlewares/validateBody";
+import userSchema from "./validation/userSchema";
+import categoriesSchema from "./validation/categoriesSchema";
+import productSchema from "./validation/productsSchema";
+import orderSchema from "./validation/orderSchema";
 
 const router = express.Router();
 
 router.post(
-  '/auth/register',
+  "/auth/register",
   validationBody(userSchema),
   authController.register
 );
 
-router.post('/auth/login', validationBody(loginSchema), authController.login);
+router.post("/auth/login", validationBody(loginSchema), authController.login);
 
 router.post(
-  '/categories',
+  "/categories",
   validationBody(categoriesSchema),
   ensureAuth,
   categoriesController.create
 );
 
-router.put('/categories/:id', ensureAuth, categoriesController.update);
+router.put("/categories/:id", ensureAuth, categoriesController.update);
 
-router.delete('/categories/:id', ensureAuth, categoriesController.delete);
+router.delete("/categories/:id", ensureAuth, categoriesController.delete);
 
-router.get('/categories/:id', ensureAuth, categoriesController.getCategoryById);
+router.get("/categories/:id", ensureAuth, categoriesController.getCategoryById);
 
-router.get('/categories', ensureAuth, categoriesController.getAllCategories);
+router.get("/categories", ensureAuth, categoriesController.getAllCategories);
 
-router.get('/user', ensureAuth, usersController.getuserDetail);
+router.get("/user", ensureAuth, usersController.getuserDetail);
 
-router.put('/user', ensureAuth, usersController.update);
+router.put("/user/:id", ensureAuth, usersController.update);
 
-router.put('/user/password', ensureAuth, usersController.updatePassword);
+router.put("/user/password", ensureAuth, usersController.updatePassword);
 
 router.post(
-  '/product',
+  "/product",
   validationBody(productSchema),
   ensureAuth,
   productController.save
 );
 
-router.put('/product/:id', ensureAuth, productController.update);
+router.put("/product/:id", ensureAuth, productController.update);
 
-router.get('/product/:id', ensureAuth, productController.getProductById);
+router.get("/product/:id", ensureAuth, productController.getProductById);
 
-router.delete('/product/:id', ensureAuth, productController.delete);
+router.delete("/product/:id", ensureAuth, productController.delete);
 
-router.get('/product', ensureAuth, productController.getAllProducts);
+router.get("/product", ensureAuth, productController.getAllProducts);
 
-router.post('/customer', ensureAuth, customerController.create);
+router.post("/customer", ensureAuth, customerController.create);
 
-router.put('/customer/:id', ensureAuth, customerController.update);
+router.put("/customer/:id", ensureAuth, customerController.update);
 
-router.get('/customer/:id', ensureAuth, customerController.getCustomerById);
+router.get("/customer/:id", ensureAuth, customerController.getCustomerById);
 
-router.get('/customer', ensureAuth, customerController.getAllCustomers);
+router.get("/customer", ensureAuth, customerController.getAllCustomers);
 
 router.post(
-  '/order',
+  "/order",
   ensureAuth,
   validationBody(orderSchema),
   orderController.create
 );
 
-router.get('/order', ensureAuth, orderController.getAllOrders);
+router.get("/order", ensureAuth, orderController.getAllOrders);
 
 export { router };
