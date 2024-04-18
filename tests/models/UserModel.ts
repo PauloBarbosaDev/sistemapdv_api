b4af8843c2ac0cb2db878d24aa8ef117990da1e3
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-04-18 03:22:54 -0300

    refactor: Update password length in test user models

--- tests/models/UserModel.ts
+++ tests/models/UserModel.ts
@@ -1,15 +1,15 @@
-import { UserCreationAttributes } from '../../src/models/User';
+import { UserCreationAttributes } from "../../src/models/User";
 
 const user: UserCreationAttributes = {
-  name: 'Paulo Barbosa',
-  email: 'pauloBarbosa@gmail.com',
-  password: 'pb4234f',
+  name: "Paulo Barbosa",
+  email: "pauloBarbosa@gmail.com",
+  password: "12345678",
 };
 
 const user2: UserCreationAttributes = {
-  name: 'Paulo Barbosa',
-  email: 'pauloBarbosa2@gmail.com',
-  password: 'pb4234f',
+  name: "Paulo Barbosa",
+  email: "pauloBarbosa2@gmail.com",
+  password: "12345678",
 };
 
 export { user, user2 };
