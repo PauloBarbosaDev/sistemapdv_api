42cc3f7eae25e738cfa8a394ed3a5621ab66a0dd
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-04-20 12:25:53 -0300

    test:Jwt no token found and Invalid token tests

--- tests/user/controllers/deleteUser.test.ts
+++ tests/user/controllers/deleteUser.test.ts
@@ -37,4 +37,29 @@ describe("Delete User Controller", () => {
 
     expect(response.statusCode).toBe(204);
   });
+
+  it("Invalid token", async () => {
+    const errorToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTYwLCJuYW1lIjoiUGF1bG8gYmFyYm9zYSIsImVtYWlsIjoidGVzdGUyQGhvdG1hYWlsLmNvbSIsImlhdCI6MTcxMzQ5NjUyNywiZXhwIjoxNzEzNTgyOTI3fQ.8D4VgSH41zSiXH66OxId5by3fthLbvsxpFY02dtB";
+
+    await deleteUser(errorToken, userIdTest);
+
+    expect(response.status).toBe(401);
+    expect(response.body).toHaveProperty(
+      "message",
+      "Unauthorized: invalid token"
+    );
+  });
+
+  it("Jwt no token found", async () => {
+    let errorToken;
+
+    await deleteUser(errorToken, userIdTest);
+
+    expect(response.status).toBe(401);
+    expect(response.body).toHaveProperty(
+      "message",
+      "Unauthorized: no token found"
+    );
+  });
 });
