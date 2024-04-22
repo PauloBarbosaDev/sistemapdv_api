5c693c2a56250aad453a8e93f04f01c68396a201
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-04-22 05:34:16 -0300

    test: Add unit test for delete function in userService

---
+++ tests/user/services/deleteUser.test.ts
@@ -0,0 +1,22 @@
+import { User, UserCreationAttributes } from "../../../src/models/User";
+import { userService } from "../../../src/services/userService";
+
+describe("Delete user", () => {
+  beforeEach(async () => {
+    await User.destroy({ where: {} });
+  });
+
+  it("Should be able to delete a user", async () => {
+    const userData: UserCreationAttributes = {
+      id: 1,
+      name: "Paulo barbosa",
+      email: "paulo_barbosa@teste.com",
+      password: "12345678",
+    };
+
+    await userService.create(userData);
+
+    const deletionResult = await userService.deleteUserById(userData.id!);
+    expect(deletionResult).toBe(1);
+  });
+});
