f9daa809f55c3e8b88e942d59920f967c2fd20eb
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-04-23 13:13:20 -0300

    test: Add CategoriesModel for testing purposes

---
+++ tests/models/categories/CategoriesModel.ts
@@ -0,0 +1,11 @@
+import { CategoryCreationAttributes } from "../../../src/models/Category";
+
+const category: CategoryCreationAttributes = {
+  description: "Description test",
+};
+
+const category2: CategoryCreationAttributes = {
+  description: "Description test2",
+};
+
+export { category, category2 };
